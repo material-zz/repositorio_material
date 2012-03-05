8a1cf1354423f90533f87e67758008e352015d9a
andre <carvalho.andrecampos@gmail.com>
2012-03-05 11:59:24 -0300

    criado alterar aluno

--- spec/acceptance/gerenciar_aluno_spec.rb
+++ spec/acceptance/gerenciar_aluno_spec.rb
@@ -20,4 +20,22 @@ feature 'gerenciar aluno' do
     page.should have_content 'Matricula: 1234'
     page.should have_content 'Curso: informatica'
   end
+
+  scenario 'alterar aluno' do
+    aluno = Aluno.create nome: 'Geraldo'
+    curso = Curso.create nome: 'informatica'
+    visit edit_aluno_path(aluno)
+    
+    fill_in 'aluno_nome', :with => 'Joao'
+    fill_in 'aluno_matricula', :with => '1234'
+    select('informatica', from: "Curso")
+  
+    click_button 'Salvar'
+   
+    page.should have_content 'Nome: Joao'
+    page.should have_content 'Matricula: 1234'
+    page.should have_content 'Curso: informatica'
+  end
+
+
 end
